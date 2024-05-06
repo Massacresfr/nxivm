e016893d488938686cf60a03f614da5958bcf2c3
Massacresfr <161874665+Massacresfr@users.noreply.github.com>
2024-05-07 02:42:11 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -19,7 +19,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Channel : @Nxivm_Network\n○ Support Group : @Nxivm_Support</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Channel : @Bloods_Siterip</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
