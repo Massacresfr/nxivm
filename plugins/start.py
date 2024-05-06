418baef9464a4d00610a892c8af7c859afd92cb9
Massacresfr <161874665+Massacresfr@users.noreply.github.com>
2024-05-07 05:01:37 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -104,8 +104,8 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
+                    InlineKeyboardButton("𝗕𝗹𝗼𝗼𝗱𝘀 𝗡𝗲𝘁𝘄𝗼𝗿𝗸 ☘️", callback_data = "about"),
+                    InlineKeyboardButton("Close 🔒", callback_data = "close")
                 ]
             ]
         )
